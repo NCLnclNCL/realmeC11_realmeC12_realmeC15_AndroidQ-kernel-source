f0b948b175f776f1e154db0a97fc3f57817357fa
NCLnclNCL <nanvanu@gmail.com>
2025-08-06 10:16:15 +0700

    Subject: [PATCH] rcu: Run nocb kthreads on little CPUs

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1500,8 +1500,8 @@ struct sched_avg {
 	u64 last_update_time, load_sum;
 	u32 util_sum, period_contrib;
 	unsigned long load_avg, util_avg;
-#ifdef CONFIG_SCHED_HMP
 	unsigned long loadwop_avg, loadwop_sum;
+#ifdef CONFIG_SCHED_HMP
 	unsigned long pending_load;
 	u32 nr_pending;
 	u32 nr_dequeuing_low_prio;
