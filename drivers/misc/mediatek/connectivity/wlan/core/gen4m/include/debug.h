d0e42e6383fc468c53bd0f8b18eb690ebd68efe0
NCLnclNCL <126905141+NCLnclNCL@users.noreply.github.com>
2025-08-09 12:33:29 +0700

    Update debug.h

--- drivers/misc/mediatek/connectivity/wlan/core/gen4m/include/debug.h
+++ drivers/misc/mediatek/connectivity/wlan/core/gen4m/include/debug.h
@@ -105,12 +105,11 @@ extern int get_logtoomuch_enable(void) __attribute__((weak));
 #define DBG_LOG_LEVEL_DEFAULT \
 	(DBG_CLASS_ERROR)
 #define DBG_LOG_LEVEL_MORE \
-	(DBG_CLASS_WARN | \
+	(DBG_LOG_LEVEL_DEFAULT | \
+    DBG_CLASS_WARN | \
 	DBG_CLASS_STATE | \
 	DBG_CLASS_EVENT | \
 	DBG_CLASS_INFO | \
-
-	DBG_LOG_LEVEL_DEFAULT | \
 	DBG_CLASS_TRACE)
 #define DBG_LOG_LEVEL_EXTREME \
 	(DBG_LOG_LEVEL_MORE | \
