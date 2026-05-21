8a6942a0bb1853ed7d83e9756d644db39f9a17b2
NCLnclNCL <126905141+NCLnclNCL@users.noreply.github.com>
2026-05-21 15:12:36 +0700

    use ksu v3

--- fs/susfs.c
+++ fs/susfs.c
@@ -19,13 +19,9 @@
 #include <linux/delay.h>
 
 static spinlock_t susfs_spin_lock;
-
+extern void setup_selinux(const char *domain, struct cred *cred);
 extern bool susfs_is_current_ksu_domain(void);
-#ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-extern void ksu_try_umount(const char *mnt, bool check_mnt, int flags, uid_t uid);
-#endif
-extern bool susfs_is_avc_log_spoofing_enabled;
-
+bool susfs_is_avc_log_spoofing_enabled =true;
 #ifdef CONFIG_KSU_SUSFS_ENABLE_LOG
 bool susfs_is_log_enabled __read_mostly = true;
 #define SUSFS_LOGI(fmt, ...) if (susfs_is_log_enabled) pr_info("susfs:[%u][%d][%s] " fmt, current_uid().val, current->pid, __func__, ##__VA_ARGS__)
@@ -1023,7 +1019,16 @@ static int susfs_sdcard_monitor_fn(void *data)
 	struct path path;
 	int err = 0, max_attempts = SDCARD_MONITOR_MAX_ATTEMPTS;
 	
-	ksu_setup_selinux("u:r:su:s0");
+	struct cred *cred = prepare_creds();
+	int ret = 0;
+
+	if (!cred) {
+		SUSFS_LOGE("failed to prepare creds!\n");
+		return -ENOMEM;
+	}
+
+	setup_selinux("u:r:ksu:s0", cred);
+
 
 	if (!susfs_is_current_ksu_domain()) {
 		SUSFS_LOGE("Domain is not su, exiting the thread\n");
@@ -1086,4 +1091,3 @@ void susfs_init(void) {
 
 /* No module exit is needed becuase it should never be a loadable kernel module */
 //void __init susfs_exit(void)
-
