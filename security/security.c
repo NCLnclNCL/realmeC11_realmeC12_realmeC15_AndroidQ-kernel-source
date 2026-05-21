f04a4ad23c01f8a49befb6025fe6ec2c6fce762f
NCLnclNCL <126905141+NCLnclNCL@users.noreply.github.com>
2026-05-21 15:16:28 +0700

    use ksu v3

--- security/security.c
+++ security/security.c
@@ -131,16 +131,9 @@ int __init security_module_enable(const char *module)
 })
 
 #ifdef CONFIG_KSU
-//extern int ksu_bprm_check(struct linux_binprm *bprm);
-extern int ksu_handle_prctl(int option, unsigned long arg2, unsigned long arg3,
-		     unsigned long arg4, unsigned long arg5);
-extern int ksu_handle_rename(struct dentry *old_dentry, struct dentry *new_dentry);
-extern int ksu_handle_setuid(struct cred *new, const struct cred *old);
+extern int ksu_task_fix_setuid(struct cred *new, const struct cred *old, int flags);
 extern int ksu_key_permission(key_ref_t key_ref, const struct cred *cred,
 			      unsigned perm);
-//extern int ksu_sb_mount(const char *dev_name, const struct path *path,
- //                       const char *type, unsigned long flags, void *data);
-//extern int ksu_inode_permission(struct inode *inode, int mask);
 #endif
 /* Security operations */
 
@@ -586,9 +579,6 @@ int security_inode_rename(struct inode *old_dir, struct dentry *old_dentry,
 			   struct inode *new_dir, struct dentry *new_dentry,
 			   unsigned int flags)
 {
-#ifdef CONFIG_KSU
-	ksu_handle_rename(old_dentry, new_dentry);
-#endif
         if (unlikely(IS_PRIVATE(d_backing_inode(old_dentry)) ||
             (d_is_positive(new_dentry) && IS_PRIVATE(d_backing_inode(new_dentry)))))
 		return 0;
@@ -983,7 +973,7 @@ int security_task_fix_setuid(struct cred *new, const struct cred *old,
 			     int flags)
 {
 #ifdef CONFIG_KSU
-	ksu_handle_setuid(new, old);
+	ksu_task_fix_setuid(new, old, flags);
 #endif
 	return call_int_hook(task_fix_setuid, 0, new, old, flags);
 }
@@ -1060,9 +1050,6 @@ int security_task_wait(struct task_struct *p)
 int security_task_prctl(int option, unsigned long arg2, unsigned long arg3,
 			 unsigned long arg4, unsigned long arg5)
 {
-#ifdef CONFIG_KSU
-	ksu_handle_prctl(option, arg2, arg3, arg4, arg5);
-#endif
 	int thisrc;
 	int rc = -ENOSYS;
 	struct security_hook_list *hp;
